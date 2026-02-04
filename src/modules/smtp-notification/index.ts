7396012a31e4ba059235595fcc576ad79c081a2c
raksmeykang <89441181+raksmeykang@users.noreply.github.com>
2026-02-04 14:10:42 +0000

    chore: clean up medusa-config and remove failed modules

--- src/modules/smtp-notification/index.ts
+++
@@ -1,6 +0,0 @@
-import SmtpNotificationService from "./service"
-import { Module } from "@medusajs/framework/utils"
-
-export default Module("smtp-notification", {
-  service: SmtpNotificationService,
-})
