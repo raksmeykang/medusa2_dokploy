5bf4415dda3423e9dfc9882ee0b48eb0efcac6c2
raksmeykang <89441181+raksmeykang@users.noreply.github.com>
2026-02-04 14:21:05 +0000

    chore: update workflow engine redis config to use redisUrl

--- backend/medusa-config.ts
+++ backend/medusa-config.ts
@@ -31,7 +31,7 @@ module.exports = defineConfig({
       resolve: "@medusajs/medusa/workflow-engine-redis",
       options: {
         redis: {
-          url: process.env.REDIS_URL,
+          redisUrl: process.env.REDIS_URL,
         },
       },
     },
