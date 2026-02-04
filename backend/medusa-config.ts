9f88f1a435682f7e6022228ec252d05e54591dce
raksmeykang <89441181+raksmeykang@users.noreply.github.com>
2026-02-04 13:58:31 +0000

    fix: add smtp module and package

--- backend/medusa-config.ts
+++ backend/medusa-config.ts
@@ -36,5 +36,28 @@ module.exports = defineConfig({
         },
       },
     },
+   {
+  resolve: "@medusajs/medusa/notification",
+  options: {
+    providers: [
+      {
+        resolve: "@medusajs/medusa/notification-smtp",
+        id: "smtp",
+        options: {
+          channels: ["email"],
+          from: process.env.SMTP_USER,
+          transport: {
+            host: process.env.SMTP_HOST,
+            port: parseInt(process.env.SMTP_PORT || "587"),
+            auth: {
+              user: process.env.SMTP_USER,
+              pass: process.env.SMTP_PASSWORD,
+            },
+          },
+        },
+      },
+    ],
+  },
+}, 
   ],
 })
