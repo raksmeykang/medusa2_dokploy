5c583df5cfe124b41b812e1a910247e8d13ed1c3
raksmeykang <89441181+raksmeykang@users.noreply.github.com>
2026-02-08 12:40:57 +0000

    fix: disable database ssl for production

--- backend/medusa-config.ts
+++ backend/medusa-config.ts
@@ -4,16 +4,22 @@ loadEnv(process.env.NODE_ENV || 'development', process.cwd())
 
 export default defineConfig({
   projectConfig: {
-    databaseUrl: process.env.DATABASE_URL,
-    redisUrl: process.env.REDIS_URL, // Global Redis reference
-    http: {
-      storeCors: process.env.STORE_CORS!,
-      adminCors: process.env.ADMIN_CORS!,
-      authCors: process.env.AUTH_CORS!,
-      jwtSecret: process.env.JWT_SECRET || "supersecret",
-      cookieSecret: process.env.COOKIE_SECRET || "supersecret",
-    }
+  databaseUrl: process.env.DATABASE_URL,
+  // Add this block to fix the SSL error
+  databaseDriverOptions: {
+    connection: {
+      ssl: false,
+    },
   },
+  redisUrl: process.env.REDIS_URL,
+  http: {
+    storeCors: process.env.STORE_CORS!,
+    adminCors: process.env.ADMIN_CORS!,
+    authCors: process.env.AUTH_CORS!,
+    jwtSecret: process.env.JWT_SECRET || "supersecret",
+    cookieSecret: process.env.COOKIE_SECRET || "supersecret",
+  }
+},
   admin: {
     // THIS LINE FIXES THE "CANNOT GET /" ERROR
     path: "/", 
