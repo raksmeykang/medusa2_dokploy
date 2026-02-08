da275abdb09ef0f7a36a18f19bb680ca36a7e3fd
raksmeykang <89441181+raksmeykang@users.noreply.github.com>
2026-02-08 05:25:13 +0000

    feat: Integrate database migrations into Dockerfile

--- backend/medusa-config.ts
+++ backend/medusa-config.ts
@@ -1,5 +1,7 @@
 import { loadEnv, defineConfig } from '@medusajs/framework/utils'
+
 loadEnv(process.env.NODE_ENV || 'development', process.cwd())
+
 module.exports = defineConfig({
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL,
@@ -16,17 +18,7 @@ module.exports = defineConfig({
     }
   },
   admin: {
+    // This allows Dokploy to control if the admin builds or not
     disable: process.env.DISABLE_MEDUSA_ADMIN === "true",
-    path: "/", // Critical: Removes /app prefix
-  },
-  modules: [
-    {
-      resolve: "@medusajs/medusa/event-bus-redis",
-      options: { redisUrl: process.env.REDIS_URL + "?family=0" },
-    },
-    {
-      resolve: "@medusajs/medusa/workflow-engine-redis",
-      options: { redis: { redisUrl: process.env.REDIS_URL + "?family=0" } },
-    },
-  ],
+  }
 })
